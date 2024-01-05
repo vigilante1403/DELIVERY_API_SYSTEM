5a3f2c8677474feeae798aa315c2df2fbf70b567
Uzikami <baohoaitran1502@gmail.com>
2024-01-05 21:17:14 +0700

    crud de-status, de-agent, get profile customer

---
+++ api/DTO/DeliveryAgentDTO.cs
@@ -0,0 +1,9 @@
+namespace api.DTO{
+    public class DeliveryAgentDTO
+    {
+        public int Id { get; set; }
+        public string AgentName { get; set; }
+        public string VehicleNumber { get; set; }
+        public string AgentContactNumber { get; set; }
+    }
+}
